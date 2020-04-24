57f0076306209ea424de80b9cd740317481c655b
Kamil Matejuk <kamilmatejuk@gmail.com>
2020-04-24 23:01:38 +0200

    Update main.c

--- C/Agent_Na_Płaszczyznie/main.c
+++ C/Agent_Na_Płaszczyznie/main.c
@@ -2,14 +2,6 @@
 #include <stdio.h>
 #include "agents.h"
 
-/* Agent porusza się na płaszczyźnie po kracie punktów o współrzędnych całkowitych.
-Agent znajdujący się w miejscu (x, y) może wykonać krok na północ i przejdzie na miejsce (x, y + 1).
-Agent znajdujący się w miejscu (x, y) może wykonać krok na południe i przejdzie na miejsce (x, y − 1).
-Agent znajdujący się w miejscu (x, y) może wykonać krok na wschód i przejdzie na miejsce (x + 1, y).
-Agent znajdujący się w miejscu (x, y) może wykonać krok na zachód i przejdzie na miejsce (x − 1, y). 
-
-Zdefiniuj strukturę Agent oraz funkcje north, south, east, west */
-
 int main()
 {
     struct agent Bob = newagent(0,0);
