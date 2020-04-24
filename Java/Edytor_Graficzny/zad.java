ae75fd5118bb27d90b0e9e41d4a13a3ada40c0f4
Kamil Matejuk <kamilmatejuk@gmail.com>
2020-04-24 23:20:27 +0200

    Update zad.java

--- Java/Edytor_Graficzny/zad.java
+++ Java/Edytor_Graficzny/zad.java
@@ -1,22 +1,10 @@
+/* Kamil Matejuk */
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.ArrayList;
-
-/* Napisz prosty edytor graczny umożliwiający tworzenie i edycję figur geometrycznych. Aplikacja powinna działać w
-dwóch trybach:
-1. Tworzenie figury geometrycznej:
-    Wybieramy z menu jedną z figur i rysujemy za pomocą myszy na panelu.
-
-2. Modyfikowanie figury geometrycznej:
-    Figure można oznaczyż jako aktywną. Wówczas powinno się dać: przesunąć ją w inne miejsce, lub powiekszyć.
-    Dodatkowo pod prawym przyciskiem myszy powinno sie pojawić menu umozliwiające zmianę koloru wypełnienia figury.
-
-Aplikacja powinna by¢ wyposażona w przycisk Info wyświetlający okienko dialogowe z informacjami o programie
-(nazwa, przeznaczenie, autor, ...).
-Aplikacja powinna umożliwić zapisanie tego co jest na ekranie do pliku i wczytanie figur z istniejącego pliku. */
 
 /**
  * Główna klasa.
