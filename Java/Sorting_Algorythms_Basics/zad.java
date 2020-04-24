f0acf0343ea0e8e24cd4da532a596288b90b4e17
Kamil Matejuk <kamilmatejuk@gmail.com>
2020-04-24 23:23:46 +0200

    Update zad.java

--- Java/Sorting_Algorythms_Basics/zad.java
+++ Java/Sorting_Algorythms_Basics/zad.java
@@ -8,22 +8,6 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-/* Zaimplementuj następujące algorytmy sortowania: Insertion Sort, Merge Sort, Quick Sort, Dual Pivot Quick Sort oraz
-stwórz własne połączenie dowolnych z nich by otrzymać najszybszy algorytm. Wybór typu sortowania odbywa się flagą --type,
-natomomiast porównnia (rosnąco/ malejąco) flagą --comp. Flaga --stat pozwala na wygenerowanie pliku ze statystyką czasu,
-ilości porównań oraz ilości zamian dla k prób.
-
-kompilacja:
-	javac *.java
-uruchamianie:
-	java zad4 --type insert|merge|quick|dualpivot|hybrid --comp '<='|'>=' --stat|--smallstat|--stringstat|--stringsmallstat filename.txt k
-gdzie:
-	--stat generuje statystyki dla liczb całkowitych i n: {100, 200, ... 1000}
-	--smallstat generuje statystyki dla liczb całkowitych i n: {2, 3, 4, ... 100}
-	--stat generuje statystyki dla ciagów alfanumerycznych i n: {100, 200, ... 1000}
-	--smallstat generuje statystyki dla ciagów alfanumerycznych i n: {2, 3, 4, ... 100}
-*/
-
 public class zad {
 
     public static final boolean STANDARD_OUT_ERR = false;
