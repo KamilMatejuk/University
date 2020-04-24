007a5390ac01ae67307dca3373ea1a05ead181a2
Kamil Matejuk <kamilmatejuk@gmail.com>
2020-04-24 23:06:12 +0200

    Update main.c

--- C/Wzorzec_w_Ciagu_Znakow/main.c
+++ C/Wzorzec_w_Ciagu_Znakow/main.c
@@ -3,16 +3,6 @@
 #include <stdio.h>
 #include <string.h>
 
-/* Napisz funkcję match(char* wzorzec, char* łańcuch), która ustala zgodność wzorca z łańcuchem.
-Znak ? oznacza zgodność z dowolnym innym znakiem. 
-Znak * oznacza zgodność z dowolnym, również pustym, ciągiem znaków w łańcuchu.
-Znak różny od ? i * oznacza zgodność tylko z samym sobą.
-Na przykład:
-    • match(”*.doc”,s) ma zwracać true wtedy i tylko wtedy, gdy napis s jest ciągiem dowolnych znaków z czterema ostatnimi znakami ’.doc’
-    • match(”a???”,s) ma zwracać true wtedy i tylko wtedy, gdy s ma długość 4 i zaczyna się od litery ’a’
-    • match("a*b*b*c", s) ma zwracać true wtedy i tylko wtedy, gdy napis s zaczyna się od litery ’a’ i kończy się literą ’c’ 
-    a między nimi znajdują się przynajmniej dwie litery ’b’ (niekoniecznie obok siebie) */
-
 int main()
 {
     char wzorzec[1000], lancuch[1000];
