3a0fb3b6ef3060a6d723d1d0c6022925bc9c63b7
Kamil Matejuk <kamilmatejuk@gmail.com>
2020-04-24 23:27:13 +0200

    Update zad.java

--- Java/Sorting_Algorythms_Intermediate/zad.java
+++ Java/Sorting_Algorythms_Intermediate/zad.java
@@ -10,18 +10,6 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-* Zaimplementuj następujące algorytmy sortowania: Insertion Sort, Merge Sort, Quick Sort oraz Radix Sort.
-Wybór typu sortowania odbywa się flagą --type, natomomiast porównnia (rosnąco/ malejąco) flagą --comp. Flaga --stat
-pozwala na wygenerowanie pliku ze statystyką czasu, ilości porównań, ilości zamian oraz wykorzystanej pamięci dla k prób.
-
-kompilacja:
-    javac *.java
-uruchamianie:
-    java zad1 --type insert|merge|quick|radix --comp '<='|'>=' --stat filename.txt k
-uwaga: sortowanie działa tylko dla liczb całkowitych.
-uwaga: ze względu na działanie Garbage Collectora pomiary zużytej pamięci są niedokładne, a czasem mogą wyjść ujemne.
-*/
-
 public class zad {
 
     public static final boolean STANDARD_OUT_ERR = false;
